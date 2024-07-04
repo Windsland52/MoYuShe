307aa45cb6592405c0ea0635a4b54303508524e9
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2024-07-05 06:37:42 +0800

    新增摸鱼社签到，获取摸鱼币

--- MoYuShe.py
+++ MoYuShe.py
@@ -1,10 +1,14 @@
 import init
 import show
 import sign
+import signMoYu
 
 def MoYuShe():
     show.get_show()
+    print("开始灵魂潮汐每日签到：")
     sign.sign()
+    print("开始摸鱼社每日签到：")
+    signMoYu.signMoYu()
 
 if __name__ == '__main__':
     try:
---
+++ signMoYu.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+token = json.load(open('config.json'))['token']
+devcode = json.load(open('config.json'))['devcode']
+
+def signMoYu():
+    url = "https://herobox.yingxiong.com:25362/user/signIn"
+
+    headers = {
+        "log-header": "I am the log request header.",
+        "countrycode": "CN",
+        "devcode": f"{devcode}",
+        "key": "FCxkCfRLV65O9+rIgo6FRfPAFp3o0j5YD+UZnd+ffc/ADdiLRnEIT2+dNBKzizPT+HHsg8ev4vNvXOzjuc3Ub/yelKXNok9zVTimirOnoPSQfpLJC//Sm/NB0U839f3UozYwItAMtlE8llYbFSygCr15OZBxhnZpTvUEYxhLsmU=",
+        "lang": "zh-Hans",
+        "model": "HD1910",
+        "osversion": "28",
+        "rv": "NmGlr5VQmyqepRvT",
+        "source": "android",
+        "token": f"{token}",
+        "version": "3.3.0",
+        "versioncode": "55",
+        "content-type": "application/x-www-form-urlencoded",
+        "accept-encoding": "gzip",
+        "user-agent": "okhttp/3.10.0"
+    }
+
+    data = {
+        "gameId": "649"
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+
+    if response.json()['code'] == 200:
+        print("签到成功！")
+    else:
+        print(f"签到失败！{response.json()['msg']}")
+
+if __name__ == '__main__':
+    signMoYu()
