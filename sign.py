cc8f6bd300b409de6ab06fb2b9f8c2c2e32d7642
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2024-07-15 00:14:13 +0800

    fix: 修改连续签到相关的bug

--- sign.py
+++ sign.py
@@ -45,16 +45,22 @@ def sign():
 
     response = requests.post(url, headers=headers, data=data)
     # print(response.text)
+    # try:
+    #     with open('response.json', 'w', encoding='utf-8') as f:
+    #             json.dump(response.json(), f, ensure_ascii=False, indent=4)
+    # except:
+    #     print("写入response.json失败！")
     if response.status_code == 200:
         if response.json()['code'] == 200:
             print(f"签到成功！获得{show_dict['data']['dayAward'][signinTime]['awardName']}")
-            if response.json()['data']['signinTimeNow'] in ["5", "10", "15", 5, 10, 15]:
+            if response.json()['data']['signinTimeNow'] in ["5", "10", "15"]:
+                # print(f"signInTimeNow类型为{type(response.json()['data']['signinTimeNow'])}") // str
                 signinTimeNow = int(response.json()['data']['signinTimeNow'])
                 continueAwardList = []
                 for item in show_dict['data']['continueAward']:
                     if item['achieveDays'] == signinTimeNow:
                         continueAwardList.append(item['awardName'])
-                print(type(response.json()['data']['sendContinueAwardName']))
+                # print(f"sendContinueAward类型为{type(response.json()['data']['sendContinueAward'])}") // bool
                 print(f"累计签到{signinTimeNow}天，获得{continueAwardList}")
         elif response.json()['code'] == 711:
             print(f"今天已经签到过了！")
